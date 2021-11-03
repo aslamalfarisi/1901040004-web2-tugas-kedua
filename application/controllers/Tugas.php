0d141bcee85e9d1c4d5df2e132bd93ecc4394de6
lom <aslamalfarisi1234@gmail.com>
2021-11-03 15:07:33 +0800

    first commit

---
+++ application/controllers/Tugas.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Tugas extends CI_Controller
+{
+    public function kedua()
+    {
+        $this->load->view('tugas/kedua');
+    }
+}
---
+++ application/views/tugas/kedua.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 2</title>
+    <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.css'); ?>">
+</head>
+
+<body>
+
+    <div class="container mt-3">
+        <div style="text-align: center;" class="alert alert-info" role="alert">
+            <h2>Tugas 2</h2>
+        </div>
+    </div>
+    <main class="container mt-3">
+        <div class="row">
+            <div class="col-6">
+                <div class="card">
+                    <div class="card-body">
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <td>Nim</td>
+                                    <td>Nama</td>
+                                    <td>Alamat</td>
+                                </tr>
+                                <tr>
+                                    <td>1901040004</td>
+                                    <td>Muhammad Aslam Alfarisi</td>
+                                    <td>Sukadana, Bayan KLU</td>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <div class="col-5">
+                <div class="card" style="width: 18rem;">
+                    <img src="<?= base_url('assets/img/profile.png'); ?>" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Muhammad Aslam Alfarisi</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+</body>
+
+</html>
